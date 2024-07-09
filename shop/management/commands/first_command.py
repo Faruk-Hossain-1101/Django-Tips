ccdd4f48b3928b2d6835555ad67c8049c98b29e1
Faruk Hossain <hossainf114@gmail.com>
2024-07-09 10:30:28 +0530

    Second command

--- shop/management/commands/first_command.py
+++ shop/management/commands/first_command.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    help="Simple cron jobs"
+    help="Simple Custom Commands"
     
     def handle(self, *args, **options):
         self.stdout.write("Hello from custom command!")
